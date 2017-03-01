8b284c41cad586cf65df01e9ca7db82d54200d17
umasuo <liuquan89@gmail.com>
2017-03-01 20:45:24 +0800

    update

--- src/main/java/com/umasuo/device/info/Application.java
+++ src/main/java/com/umasuo/device/info/Application.java
@@ -1,20 +1,26 @@
-package com.umasuo;
+package com.umasuo.device.info;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by umasuo on 17/2/9.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.umasuo")
 @RestController
+@Configuration
 public class Application {
 
-  @GetMapping("/test")
-  public String testApi() {
-    return "test";
+  @Value("${spring.application.name}")
+  private String serviceName;
+
+  @GetMapping("/")
+  public String getServiceName() {
+    return serviceName + " : " + System.currentTimeMillis();
   }
 
   public static void main(String[] args) {
