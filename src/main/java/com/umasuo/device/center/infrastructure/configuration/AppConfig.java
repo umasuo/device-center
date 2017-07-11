2ca9cd197565bfee652e3f24c001e5781ff722a6
davis999 <davis.dai@go6d.com>
2017-07-11 15:32:00 +0800

    edit config url

--- src/main/java/com/umasuo/device/center/infrastructure/configuration/AppConfig.java
+++ src/main/java/com/umasuo/device/center/infrastructure/configuration/AppConfig.java
@@ -14,25 +14,25 @@ public class AppConfig {
   /**
    * message broker's host.
    */
-  @Value("${message-broker.host:127.0.0.1}")
+  @Value("${messagebroker.host:127.0.0.1}")
   public String msgBrokerHost;
 
   /**
    * message broker's port
    */
-  @Value("${message-broker.port:1883}")
+  @Value("${messagebroker.port:1883}")
   public long msgBrokerPort;
 
   /**
    * super user's username for message broker.
    */
-  @Value("${message-broker.username:umasuo}")
+  @Value("${messagebroker.username:umasuo}")
   public String username;
 
   /**
    * super user's password for message broker.
    */
-  @Value("${message-broker.password:password}")
+  @Value("${messagebroker.password:password}")
   public String password;
 
 }
