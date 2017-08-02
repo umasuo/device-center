76014297f797c2dffb9af5418beee3c93882eb00
davis999 <davis.dai@go6d.com>
2017-08-02 15:42:13 +0800

    edit docker repo

--- deploy/docker_pusher.sh
+++ deploy/docker_pusher.sh
@@ -13,6 +13,6 @@ echo ${BRANCH}
 echo "====================================================================="
 if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "dev" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
-  docker push umasuo/device-center;
+  docker push evacloud/device-center;
 fi
 echo "============= end push docker image ================================="
